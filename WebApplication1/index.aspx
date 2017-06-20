c02af2fe5a6097b114ecb5f8dca77c5c7b84e708
Kefentse Mathibe <KMathibe@devel.iress.com.au>
2017-06-20 10:17:26 +0200

    ola

---
+++ WebApplication1/index.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="WebApplication1.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p>
+            Information Capture</p>
+        <p>
+            Please provide the following information :</p>
+        <p style="width: 704px">
+            User ID&nbsp; :<asp:TextBox ID="TextBox4" runat="server" ValidateRequestMode="Enabled" ></asp:TextBox>
+            <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" Height="16px" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+            </asp:DropDownList>
+        </p>
+        <p>
+            First Name :
+            <asp:TextBox ID="TextBox1" runat="server" ></asp:TextBox>
+           
+        </p>
+        <p>
+            Last Name :<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Age :<asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Insert" />
+            <asp:Label ID="Label1" runat="server" ForeColor="#000099" Text="Label" Visible="False"></asp:Label>
+        </p>
+            <asp:Button ID="Button3" runat="server" Text="Update" OnClick="Button3_Click" />
+        <asp:Label ID="Label2" runat="server" ForeColor="#000099" Text="Label" Visible="False"></asp:Label>
+        &nbsp;&nbsp;&nbsp;
+        <p>
+        <asp:Button ID="Button2" runat="server" Text="Delete" OnClick="Button2_Click" />
+            <asp:Label ID="Label3" runat="server" ForeColor="#000099" Text="Label" Visible="False"></asp:Label>
+        </p>
+        <p>
+            <asp:Label ID="Label4" runat="server" ForeColor="#000099" Text="Label" Visible="False"></asp:Label>
+        </p>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataSourceID="SqlDataSource1" EmptyDataText="There are no data records to display.">
+            <Columns>
+                <asp:BoundField DataField="userID" HeaderText="userID" ReadOnly="True" SortExpression="userID" />
+                <asp:BoundField DataField="Fname" HeaderText="Fname" SortExpression="Fname" />
+                <asp:BoundField DataField="Lname" HeaderText="Lname" SortExpression="Lname" />
+                <asp:BoundField DataField="Age" HeaderText="Age" SortExpression="Age" />
+            </Columns>
+        </asp:GridView>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:Test1ConnectionString1 %>" ProviderName="<%$ ConnectionStrings:Test1ConnectionString1.ProviderName %>" SelectCommand="SELECT [userID], [Fname], [Lname], [Age] FROM [People]"></asp:SqlDataSource>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/index.aspx.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace WebApplication1
+{
+    public partial class index : System.Web.UI.Page
+    {
+        
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Test1ConnectionString1"].ConnectionString);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.Open();
+
+            if (!IsPostBack)
+            {
+
+                SqlDataAdapter da = new SqlDataAdapter("select * from People", con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                DropDownList1.DataSource = dt;
+                DropDownList1.DataTextField = "userID";
+                DataBind();
+                
+
+            }
+        }
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+            SqlCommand cmd = new SqlCommand("insert into People values('" + TextBox4.Text+"','"+TextBox1.Text+"','"+TextBox2.Text+"','"+TextBox3.Text+"')",con) ;
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Label1.Visible = true;
+            Label1.Text = "Your Data stored successfully!";
+            TextBox4.Text = "";
+            TextBox3.Text = "";
+            TextBox2.Text = "";
+            TextBox1.Text = "";
+
+            Label2.Visible = false;
+            Label3.Visible = false;
+            Label4.Visible = false;
+
+            SqlDataAdapter da = new SqlDataAdapter("select * from People ", con);
+            DataTable dt = new DataTable();
+            DataBind();
+            con.Close();
+            Label4.Visible = true;
+            Label4.Text = "Your Data successfully refreshed!";
+
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("update People set Fname='" +TextBox1.Text + "',Lname='" + TextBox2.Text + "',Age='" + TextBox3.Text + "'where UserID='" + DropDownList1.Text + "'", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Label2.Visible = true;
+            Label2.Text = "Your Data updated successfully!";
+            TextBox4.Text = "";
+            TextBox3.Text = "";
+            TextBox2.Text = "";
+            TextBox1.Text = "";
+
+            Label1.Visible = false;
+            Label3.Visible = false;
+            Label4.Visible = false;
+
+            SqlDataAdapter da = new SqlDataAdapter("select * from People ", con);
+            DataTable dt = new DataTable();
+            DataBind();
+            con.Close();
+            Label4.Visible = true;
+            Label4.Text = "Your Data successfully refreshed!";
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Label4.Visible = false;
+            Label2.Visible = false;
+            Label1.Visible = false;
+            Label3.Visible = false;
+            SqlDataAdapter da = new SqlDataAdapter("select * from People where UserID='" + DropDownList1.Text+"'", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            TextBox4.Text = dt.Rows[0][0].ToString();
+            TextBox1.Text = dt.Rows[0][1].ToString();
+            TextBox2.Text = dt.Rows[0][2].ToString();
+            TextBox3.Text = dt.Rows[0][3].ToString();
+
+            
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("Delete from People where UserID='" + DropDownList1.Text + "'", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Label3.Visible = true;
+            Label3.Text = "Your Data Deleted successfully!";
+            TextBox4.Text = "";
+            TextBox3.Text = "";
+            TextBox2.Text = "";
+            TextBox1.Text = "";
+
+            Label2.Visible = false;
+            Label1.Visible = false;
+            Label4.Visible = false;
+
+            SqlDataAdapter da = new SqlDataAdapter("select * from People ", con);
+            DataTable dt = new DataTable();
+            DataBind();
+            con.Close();
+            Label4.Visible = true;
+            Label4.Text = "Your Data successfully refreshed!";
+
+        }
+    }
+}
